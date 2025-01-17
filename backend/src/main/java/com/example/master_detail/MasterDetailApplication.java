80dae01398241aaf6c589e73588635a72117eaf2
Natalya Selyunina <seluninanv@mail.ru>
2025-01-17 14:48:47 +0300

    feat files application.properties and other files

---
+++ backend/src/main/java/com/example/master_detail/MasterDetailApplication.java
@@ -0,0 +1,12 @@
+package com.example.master_detail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDetailApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/example/master_detail/MasterDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.master_detail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
