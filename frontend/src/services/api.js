e55fef116cdabc50d15596a3b155b11dcb7a370b
Natalya Selyunina <seluninanv@mail.ru>
2025-01-20 23:52:52 +0300

    feat update, create, delete for specification and document in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,12 +5,37 @@ const api = axios.create({
 });
 
 export const getDocuments = async () => {
-    const response = await api.get('/document');
+    const response = await api.get('/document/all');
     return response.data;
 };
 
 export const createDocument = async (document) => {
-    const response = await api.post('/document', document);
+    const response = await api.post('/docs/document', document);
+    return response.data;
+};
+
+export const createSpecification = async (specification, id) => {
+    const response = await api.post(`/docs/specification/${id}`, specification);
+    return response.data;
+};
+
+export const updateDocument = async (document) => {
+    const response = await api.put('/document', document);
+    return response.data;
+};
+
+export const deleteDocument = async (id) => {
+    const response = await api.delete(`/document/${id}`);
+    return response.data;
+};
+
+export const updateSpecification = async (specification) => {
+    const response = await api.put('/docs/specification', specification);
+    return response.data;
+};
+
+export const deleteSpecification = async (id) => {
+    const response = await api.delete(`/docs/specification/${id}`);
     return response.data;
 };
 
